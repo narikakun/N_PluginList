61e94717c16b866387ece385773b0fe01011dd88
なりかくん <narikakun@gmail.com>
2021-08-09 11:39:03 +0900

    Initial commit

---
+++ src/main/java/io/github/narikakun/mc/n_pluginlist/N_PluginList.java
@@ -0,0 +1,17 @@
+package io.github.narikakun.mc.n_pluginlist;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class N_PluginList extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
